69af1e01b0f69855996ea54792f17a99c79245c8
upat <up4.3at@gmail.com>
2021-05-01 20:20:53 +0900

    グラインダー選択時の動作を変更。また、手入力とほぼ同等の速度まで最適化

--- Exchange_Excube.ino
+++ Exchange_Excube.ino
@@ -1,10 +1,11 @@
 #include "DigiKeyboard.h"
 
 /* 上記で定義されていないキーを定義 */
-#define KEY_ESC         0x29 /* ESCAPE */
-#define KEY_ARROW_RIGHT 0x4F /* 右矢印キー */
+#define KEY_ESC     0x29 /* ESCAPE */
+#define KEY_RESERVE 0x00 /* 未定義キー(Shiftのみ入力用) */
 
-#define TRADE_COUNT     1    /* 交換ループ回数 */
+#define TRADE_COUNT 30   /* 交換ループ回数(1スタック=交換上限33個*30回+9個*1回) */
+#define KEY_COUNT   16   /* 入力キー数 */
 
 // ◆動作条件
 // ・エクスキューブ交換ショップ店員に話しかけられる位置にいること
@@ -15,7 +16,7 @@
 // ・キーバインドが初期設定のものと同等であること(話しかける時など)
 
 // ◆使い方
-// ・TRADE_COUNTを設定(キューブ1スタックなら30回程度？)
+// ・TRADE_COUNTを設定(キューブ1スタックなら30回)
 // ・digisparkにこのスケッチを書き込む(終了後キー入力が実行されるので注意)
 // ・動作条件を満たした状態でUSB端子にdigisparkを差し込む
 
@@ -25,6 +26,44 @@
 /* 一度だけ動作させるためのフラグ */
 uint8_t one_flag = 0;
 
+/* 操作テーブル①(キー) */
+const byte KEYS[KEY_COUNT] = {
+  KEY_E,           /* 話かける */
+  KEY_ENTER,       /* エクスキューブ交換ショップを選択 */
+  KEY_ARROW_LEFT,  /* グラインダー33個選択 */
+  KEY_ENTER,
+  KEY_ENTER,       /* 交換確認：はい */
+  KEY_ENTER,       /* 交換に成功しました：はい */
+  KEY_ESC,         /* エクスキューブ交換ショップ閉じる */
+  KEY_ESC,
+  KEY_ESC,         /* メニュー開く */
+  KEY_ENTER,       /* アイテム選択 */
+  KEY_RESERVE,     /* グラインダー全選択 */
+  KEY_ENTER,
+  KEY_ENTER,       /* 倉庫にあずける */
+  KEY_ENTER,       /* 基本倉庫を選択 */
+  KEY_ESC,         /* メニューを閉じる */
+  KEY_ESC };
+
+/* 操作テーブル②(ディレイ時間) */
+const uint16_t DELAYS[KEY_COUNT] = {
+  800, /* 話しかけた際の画面遷移時間を考慮 */
+  250,
+  250,
+  250,
+  250,
+  250,
+  250,
+  250,
+  250,
+  250,
+  250,
+  250,
+  250,
+  250,
+  250,
+  250 };
+
 void setup()
 {
   /* 何もしない */
@@ -33,51 +72,34 @@ void setup()
 void loop()
 {
   DigiKeyboard.delay( 1000 );
-  
+
+  /* 既に動作済み? */
   if( 0 == one_flag )
   {
-    for( uint8_t loop_cnt = TRADE_COUNT; loop_cnt > 0; loop_cnt-- )
+    /* TRADE_COUNTの設定回数ループ処理 */
+    for( uint8_t loop_cnt = 0; loop_cnt < TRADE_COUNT; loop_cnt++ )
     {
-      /* 話かける */
-      sendKeyStroke_sub( KEY_E );
-      /* エクスキューブ交換ショップを選択 */
-      sendKeyStroke_sub( KEY_ENTER );
-      /* グラインダー33個選択 */
-      sendKeyStroke_sub( KEY_ARROW_LEFT );
-      sendKeyStroke_sub( KEY_ENTER );
-      /* 交換確認：はい */
-      sendKeyStroke_sub( KEY_ENTER );
-      /* 交換に成功しました：はい */
-      sendKeyStroke_sub( KEY_ENTER );
-      /* エクスキューブ交換ショップ閉じる */
-      sendKeyStroke_sub( KEY_ESC );
-      sendKeyStroke_sub( KEY_ESC );
-      /* メニュー開く */
-      sendKeyStroke_sub( KEY_ESC );
-      /* アイテム選択 */
-      sendKeyStroke_sub( KEY_ENTER );
-      /* グラインダー選択 */
-      sendKeyStroke_sub( KEY_ENTER );
-      /* 倉庫にあずける */
-      sendKeyStroke_sub( KEY_ENTER );
-      /* 基本倉庫を選択 */
-      sendKeyStroke_sub( KEY_ENTER );
-      /* 個数を選択(990個) */
-      sendKeyStroke_sub( KEY_ARROW_RIGHT );
-      sendKeyStroke_sub( KEY_ENTER );
-      sendKeyStroke_sub( KEY_ENTER );
-      /* メニューを閉じる */
-      sendKeyStroke_sub( KEY_ESC );
-      sendKeyStroke_sub( KEY_ESC );
+      /* 操作テーブルのループ処理 */
+      for( uint8_t loop_key = 0; loop_key < KEY_COUNT; loop_key++ )
+      {
+        /* 未定義キー? */
+        if( KEY_RESERVE == KEYS[loop_key] )
+        {
+          /* Shiftのみ入力 */
+          DigiKeyboard.sendKeyStroke( KEY_RESERVE, MOD_SHIFT_LEFT );
+        }
+        else
+        {
+          /* それ以外の入力 */
+          DigiKeyboard.sendKeyStroke( KEYS[loop_key] );
+        }
+
+        /* ディレイ */
+        DigiKeyboard.delay( DELAYS[loop_key] );
+      }
     }
     one_flag++;
   }
 
   DigiKeyboard.delay( 5000 );
-}
-
-void sendKeyStroke_sub( uint8_t key )
-{
-  DigiKeyboard.sendKeyStroke( key );
-  DigiKeyboard.delay( 750 ); /* これ以上早くすると事故る(750ms) */
 }
